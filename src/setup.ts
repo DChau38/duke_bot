b4dd75163de86838c20637ff410386062e30ef27
Duksing <chauduksing2@gmail.com>
2024-12-08 12:07:43 -0500

    Add optional argument postpone to /sleep so that they can postpone the deletion of their current sleep entry

--- src/setup.ts
+++ src/setup.ts
@@ -47,15 +47,21 @@ const commands = [
             {
                 type: 3, // Type 3 is for a string
                 name: "sorting_argument",
-                description:"Optionally provide an argument for how the list should be sorted",
-                required:false,
+                description: "Optional: Examples: a/alphabetize",
+                required: false,
 
             },
             {
                 type: 3,
                 name: 'timezone',
-                description: "Examples: EST,PDF,UTC...",
-                required:false,
+                description: "Optional: Examples: EST (default),PDF,UTC...",
+                required: false,
+            },
+            {
+                type: 3,
+                name: 'postpone',
+                description: 'Optional: Enter "yes" to retain current sleep record 15m+ later',
+                required: false,
             }
         ],
     },
@@ -79,19 +85,19 @@ const commands = [
                 type: 3, // string
                 name: 'description',
                 description: 'The description for the timer alert',
-                requried:true,
+                requried: true,
             },
             {
                 type: 4, // integer
                 name: 'hours',
                 description: 'The amount of hours you want added to the timer',
-                requried:false,
+                requried: false,
             },
             {
                 type: 4, // integer
                 name: 'minutes',
                 description: 'The amount of minutes you want added to the timer',
-                requried:false,
+                requried: false,
             },
 
         ]
@@ -114,7 +120,7 @@ export const client = new Client({
 });
 
 // Tracker structure: Maps server ID to a map of users and their associated time
-export const tracker: Map<string,Map<string, string | null>> = new Map();
+export const tracker: Map<string, Map<string, string | null>> = new Map();
 
 // Async function for login with error handling
 export async function startBot() {
